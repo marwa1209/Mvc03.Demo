f34bd996c244fb8b25fbd28bbd17834735280ca2
Marwa Mahmoud <marwa@bluskyint.com>
2024-09-18 11:39:38 +0300

    Add project files.

---
+++ Mvc03.Demo.PL/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mvc03.Demo.PL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
