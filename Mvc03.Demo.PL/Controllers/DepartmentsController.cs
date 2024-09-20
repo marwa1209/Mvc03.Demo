c79a629d57106e9b30e31c4ebdfa4b9c1cbafef0
marwa172 <marwa@bluskyint.com>
2024-09-20 21:38:00 +0300

    Mvc Assignment03

--- Mvc03.Demo.PL/Controllers/DepartmentsController.cs
+++ Mvc03.Demo.PL/Controllers/DepartmentsController.cs
@@ -23,6 +23,7 @@ namespace Mvc03.Demo.PL.Controllers
             return View();
         }
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public IActionResult Create(Department model)
         {
             if (ModelState.IsValid)
@@ -33,7 +34,62 @@ namespace Mvc03.Demo.PL.Controllers
                     return RedirectToAction(nameof(Index));
                 }
             }
-            return View(model); 
+            return View(model);
         }
+        [HttpGet]
+        public IActionResult Details(int? Id)
+        {
+            if (Id is null) return BadRequest();
+            var department = _departmentRepository.GetDepartment(Id.Value);
+            if (department is null) return BadRequest();
+            return View(department);
+        }
+        [HttpGet]
+        public IActionResult Update(int? Id)
+        {
+            if (Id is null) return BadRequest();
+            var department = _departmentRepository.GetDepartment(Id.Value);
+            if (department is null) return BadRequest();
+            return View(department);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update([FromRoute] int? id, Department model)
+        {
+            if (id != model.Id) return BadRequest();
+            if (ModelState.IsValid)
+            {
+                var Count = _departmentRepository.Update(model);
+                if (Count > 0)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return View(model);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                var department = _departmentRepository.GetDepartment(id);
+                if (department == null) return NotFound();
+
+                var Count = _departmentRepository.Delete(department);
+                if (Count > 0)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, ex.Message);
+            }
+
+            return BadRequest();
+        }
+
+
     }
 }
