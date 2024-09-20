f74dadc5ba35f77c6ed208ed51cdb21e2dff9ee4
marwa172 <marwa@bluskyint.com>
2024-09-20 22:57:32 +0300

    Employee Repository BLL

---
+++ Mvc03.Demo.PLL/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,18 @@
+﻿using Mvc.Demo.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mvc03.Demo.BLL.Interfaces
+{
+    public interface IEmployeeRepository
+    {
+        IEnumerable<Employee> GetAll();
+        Employee GetEmployee(int? Id);
+        int Add(Employee entity);
+        int Update(Employee entity);
+        int Delete(Employee entity);
+    }
+}
---
+++ Mvc03.Demo.PLL/Repositories/EmployeeRepository.cs
@@ -0,0 +1,49 @@
+﻿using Mvc.Demo.DAL.Data.Contexts;
+using Mvc.Demo.DAL.Models;
+using Mvc03.Demo.BLL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace Mvc03.Demo.BLL.Repositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly AppDBContext _dbContext;
+        public EmployeeRepository(AppDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IEnumerable<Employee> GetAll()
+        {
+            return _dbContext.Employees.ToList();
+        }
+
+        public Employee GetEmployee(int? Id)
+        {
+            return _dbContext.Employees.Find(Id);
+        }
+
+        public int Add(Employee entity)
+        {
+            _dbContext.Employees.Add(entity);
+            return _dbContext.SaveChanges();
+        }
+
+        public int Delete(Employee entity)
+        {
+            _dbContext.Employees.Remove(entity);
+            return _dbContext.SaveChanges();
+        }
+
+        public int Update(Employee entity)
+        {
+            _dbContext.Employees.Update(entity);
+            return _dbContext.SaveChanges();
+        }
+    }
+}
