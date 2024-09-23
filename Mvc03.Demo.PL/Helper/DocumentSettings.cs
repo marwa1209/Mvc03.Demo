a6e1d43099de1ae7ae120ca0fa0582bdbce09e5c
Marwa Mahmoud <marwa@bluskyint.com>
2024-09-23 17:51:36 +0300

    document settings

--- Mvc03.Demo.PL/Helper/DocumentSettings.cs
+++ Mvc03.Demo.PL/Helper/DocumentSettings.cs
@@ -3,7 +3,7 @@
     public class DocumentSettings
     {
         //1.Upload
-        public static Upload(IFormFile formFile ,string FolderName)
+        public static string Upload(IFormFile formFile, string FolderName)
         {
             //1.Get location of folder
             //string FolderPath =$"C:\\Users\\precision\\Desktop\\.Net\\MVC\\Mvc03.Demo.Solution\\Mvc03.Demo.PL\\wwwroot\\Files\\{FolderName}";
@@ -13,10 +13,21 @@
             //2.GetFileName and make sure it is unique
             string fileName = $"{Guid.NewGuid()}{formFile.FileName}";
             //3.Get FilePath
-            string FilePath=Path.Combine(FolderPath, fileName);
-            //4.File Stream
-            var FileStream = new FileStream(FilePath,FileMode.Create);
+            string FilePath = Path.Combine(FolderPath, fileName);
+            //4.File Stream data per sec
+            using var FileStream = new FileStream(FilePath, FileMode.Create);
             formFile.CopyTo(FileStream);
+
+            return fileName;
+        }
+        
+        //1.Delete
+        public static void Delete(string fileName, string FolderName)
+        {
+            string filePath = Path.Combine(Directory.GetCurrentDirectory(), $"\\wwwroot\\Files\\{FolderName}", fileName);
+            if (File.Exists(filePath)) {
+                File.Delete(filePath);
+            }
 
         }
     }
