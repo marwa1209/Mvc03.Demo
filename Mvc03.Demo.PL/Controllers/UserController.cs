dd7933e60cf6bacb685e85779ce314812b639077
Marwa Mahmoud <marwa@bluskyint.com>
2024-09-26 18:00:23 +0300

    to be continued

---
+++ Mvc03.Demo.PL/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Mvc.Demo.DAL.Models;
+using Mvc03.Demo.PL.ViewModels.Employees;
+using Mvc03.Demo.PL.ViewModels.User;
+
+namespace Mvc03.Demo.PL.Controllers
+{
+    public class UserController : Controller
+    {
+        public UserManager<ApplicationUser> UserManager { get; }
+        public SignInManager<ApplicationUser> SignInManager { get; }
+
+        public UserController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            UserManager = userManager;
+            SignInManager = signInManager;
+        }
+        public async Task<IActionResult> Index(string searchString)
+        {
+            var users = Enumerable.Empty<UserViewModel>();
+            if (string.IsNullOrEmpty(searchString))
+            {
+            }
+            else
+            {
+            }
+            return View();
+        }
+    }
+}
